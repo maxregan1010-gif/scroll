3c6509ac27499834a6a36d2eaf6cddf5137b535c
ilove26 <maxregan1010@gmail.com>
2026-08-26 19:10:14 +0330

    Add files via upload

---
+++ media.js
@@ -0,0 +1,275 @@
+const mediaList = [
+
+    /*
+        ==========================
+        فیلم ها
+        ==========================
+    */
+
+    {
+        id: "video-001",
+        type: "video",
+        src: "کلیپ طنز تابستون.mp4"
+    },
+
+    {
+        id: "video-002",
+        type: "video",
+        src: "کلیپ گیم شهرسازی با اهنگ ای غم بیا.mp4"
+    },
+
+    {
+        id: "video-003",
+        type: "video",
+        src: "x-files.mp4"
+    },
+
+        {
+        id: "video-004",
+        type: "video",
+        src: "ricardo.mp4"
+    },
+
+        {
+        id: "video-005",
+        type: "video",
+        src: "زیست صفار قسمت 3.mp4"
+    },
+
+        {
+        id: "video-006",
+        type: "video",
+        src: "زیست صفار قسمت 4.mp4"
+    },
+
+        {
+        id: "video-007",
+        type: "video",
+        src: "زیست صفار قسمت 1.mp4"
+    },
+
+        {
+        id: "video-008",
+        type: "video",
+        src: "زیست صفار قسمت 2.mp4"
+    },
+
+
+    /*
+        ==========================
+        عکس ها
+        ==========================
+    */
+
+    {
+        id: "image-001",
+        type: "image",
+        src: "sharifi1  (1).png"
+    },
+
+    {
+        id: "image-002",
+        type: "image",
+        src: "sharifi1  (2).png"
+    },
+
+    {
+        id: "image-003",
+        type: "image",
+        src: "sharifi1  (3).png"
+    },
+
+    {
+        id: "image-004",
+        type: "image",
+        src: "0.png"
+    },
+
+        {
+        id: "image-005",
+        type: "image",
+        src: "1.png"
+    },
+
+        {
+        id: "image-006",
+        type: "image",
+        src: "2.png"
+    },
+
+        {
+        id: "image-007",
+        type: "image",
+        src: "3.png"
+    },
+
+        {
+        id: "image-008",
+        type: "image",
+        src: "4.png"
+    },
+
+        {
+        id: "image-009",
+        type: "image",
+        src: "5.png"
+    },
+        {
+        id: "image-010",
+        type: "image",
+        src: "6.png"
+    },
+
+        {
+        id: "image-011",
+        type: "image",
+        src: "7.png"
+    },
+
+    {
+        id: "image-012",
+        type: "image",
+        src: "8.png"
+    },
+            {
+        id: "image-013",
+        type: "image",
+        src: "9.png"
+    },
+            {
+        id: "image-014",
+        type: "image",
+        src: "10.png"
+    },
+            {
+        id: "image-015",
+        type: "image",
+        src: "11.png"
+    },
+            {
+        id: "image-016",
+        type: "image",
+        src: "12.png"
+    },
+            {
+        id: "image-017",
+        type: "image",
+        src: "13.png"
+    },
+            {
+        id: "image-018",
+        type: "image",
+        src: "14.png"
+    },
+            {
+        id: "image-019",
+        type: "image",
+        src: "15.png"
+    },
+    {
+        id: "image-020",
+        type: "image",
+        src: "16.png"
+    },
+        {
+        id: "image-021",
+        type: "image",
+        src: "17.png"
+    },
+        {
+        id: "image-022",
+        type: "image",
+        src: "18.png"
+    },
+    {
+        id: "image-023",
+        type: "image",
+        src: "19.png"
+    },
+    {
+        id: "image-024",
+        type: "image",
+        src: "20.png"
+    },
+        {
+        id: "image-025",
+        type: "image",
+        src: "21.png"
+    },
+        {
+        id: "image-026",
+        type: "image",
+        src: "22.png"
+    },
+        {
+        id: "image-027",
+        type: "image",
+        src: "23.png"
+    },
+        {
+        id: "image-028",
+        type: "image",
+        src: "24.png"
+    },
+        {
+        id: "image-029",
+        type: "image",
+        src: "25.png"
+    },
+    {
+        id: "image-030",
+        type: "image",
+        src: "26.png"
+    },
+        {
+        id: "image-031",
+        type: "image",
+        src: "27.png"
+    },
+        {
+        id: "image-032",
+        type: "image",
+        src: "28.png"
+    },
+        {
+        id: "image-033",
+        type: "image",
+        src: "29.png"
+    },
+    {
+        id: "image-034",
+        type: "image",
+        src: "30.png"
+    },
+        {
+        id: "image-035",
+        type: "image",
+        src: "31.png"
+    },
+        {
+        id: "image-036",
+        type: "image",
+        src: "32.png"
+    },
+        {
+        id: "image-037",
+        type: "image",
+        src: "33.png"
+    },
+        {
+        id: "image-038",
+        type: "image",
+        src: "34.png"
+    },
+        {
+        id: "image-039",
+        type: "image",
+        src: "35.png"
+    },
+        {
+        id: "image-040",
+        type: "image",
+        src: "36.png"
+    },
+
+];
