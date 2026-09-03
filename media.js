09fff9a8370a1cd377251df9612236dc97fe4d24
ilove26 <maxregan1010@gmail.com>
2026-09-02 20:13:38 -1200

    Update media.js

--- media.js
+++ media.js
@@ -271,5 +271,473 @@ const mediaList = [
         type: "image",
         src: "36.png"
     },
+    {
+        id: "image-041",
+        type: "image",
+        src: "37.png"
+    },
+
+    {
+        id: "image-042",
+        type: "image",
+        src: "38.png"
+    },
+
+    {
+        id: "image-043",
+        type: "image",
+        src: "39.png"
+    },
+
+    {
+        id: "image-044",
+        type: "image",
+        src: "40.png"
+    },
+
+    {
+        id: "image-045",
+        type: "image",
+        src: "41.png"
+    },
+
+    {
+        id: "image-046",
+        type: "image",
+        src: "42.png"
+    },
+
+    {
+        id: "image-047",
+        type: "image",
+        src: "43.png"
+    },
+
+    {
+        id: "image-048",
+        type: "image",
+        src: "44.png"
+    },
+
+    {
+        id: "image-049",
+        type: "image",
+        src: "45.png"
+    },
+
+    {
+        id: "image-050",
+        type: "image",
+        src: "46.png"
+    },
+
+    {
+        id: "image-051",
+        type: "image",
+        src: "47.png"
+    },
+
+    {
+        id: "image-052",
+        type: "image",
+        src: "48.png"
+    },
+
+    {
+        id: "image-053",
+        type: "image",
+        src: "49.png"
+    },
+
+    {
+        id: "image-054",
+        type: "image",
+        src: "50.png"
+    },
+
+    {
+        id: "image-055",
+        type: "image",
+        src: "51.png"
+    },
+
+    {
+        id: "image-056",
+        type: "image",
+        src: "52.png"
+    },
+
+    {
+        id: "image-057",
+        type: "image",
+        src: "53.png"
+    },
+
+    {
+        id: "image-058",
+        type: "image",
+        src: "54.png"
+    },
+
+    {
+        id: "image-059",
+        type: "image",
+        src: "55.png"
+    },
+
+    {
+        id: "image-060",
+        type: "image",
+        src: "56.png"
+    },
+
+    {
+        id: "image-061",
+        type: "image",
+        src: "57.png"
+    },
+
+    {
+        id: "image-062",
+        type: "image",
+        src: "58.png"
+    },
+
+    {
+        id: "image-063",
+        type: "image",
+        src: "59.png"
+    },
+
+    {
+        id: "image-064",
+        type: "image",
+        src: "60.png"
+    },
+
+    {
+        id: "image-065",
+        type: "image",
+        src: "61.png"
+    },
+
+    {
+        id: "image-066",
+        type: "image",
+        src: "62.png"
+    },
+
+    {
+        id: "image-067",
+        type: "image",
+        src: "63.png"
+    },
+
+    {
+        id: "image-068",
+        type: "image",
+        src: "64.png"
+    },
+
+    {
+        id: "image-069",
+        type: "image",
+        src: "65.png"
+    },
+
+    {
+        id: "image-070",
+        type: "image",
+        src: "66.png"
+    },
+
+    {
+        id: "image-071",
+        type: "image",
+        src: "67.png"
+    },
+
+    {
+        id: "image-072",
+        type: "image",
+        src: "68.png"
+    },
+
+    {
+        id: "image-073",
+        type: "image",
+        src: "69.png"
+    },
+
+    {
+        id: "image-074",
+        type: "image",
+        src: "70.png"
+    },
+
+    {
+        id: "image-075",
+        type: "image",
+        src: "71.png"
+    },
+
+    {
+        id: "image-076",
+        type: "image",
+        src: "72.png"
+    },
+
+    {
+        id: "image-077",
+        type: "image",
+        src: "73.png"
+    },
+
+    {
+        id: "image-078",
+        type: "image",
+        src: "74.png"
+    },
+
+    {
+        id: "image-079",
+        type: "image",
+        src: "75.png"
+    },
+
+    {
+        id: "image-080",
+        type: "image",
+        src: "76.png"
+    },
+
+    {
+        id: "image-081",
+        type: "image",
+        src: "77.png"
+    },
+
+    {
+        id: "image-082",
+        type: "image",
+        src: "78.png"
+    },
+
+    {
+        id: "image-083",
+        type: "image",
+        src: "79.png"
+    },
+
+    {
+        id: "image-084",
+        type: "image",
+        src: "80.png"
+    },
+
+    {
+        id: "image-085",
+        type: "image",
+        src: "81.png"
+    },
+
+    {
+        id: "image-086",
+        type: "image",
+        src: "82.png"
+    },
+
+    {
+        id: "image-087",
+        type: "image",
+        src: "83.png"
+    },
+
+    {
+        id: "image-088",
+        type: "image",
+        src: "84.png"
+    },
+
+    {
+        id: "image-089",
+        type: "image",
+        src: "85.png"
+    },
+
+    {
+        id: "image-090",
+        type: "image",
+        src: "86.png"
+    },
+
+    {
+        id: "image-091",
+        type: "image",
+        src: "87.png"
+    },
+
+    {
+        id: "image-092",
+        type: "image",
+        src: "88.png"
+    },
+
+    {
+        id: "image-093",
+        type: "image",
+        src: "89.png"
+    },
+
+    {
+        id: "image-094",
+        type: "image",
+        src: "90.png"
+    },
+
+    {
+        id: "image-095",
+        type: "image",
+        src: "91.png"
+    },
+
+    {
+        id: "image-096",
+        type: "image",
+        src: "92.png"
+    },
+
+    {
+        id: "image-097",
+        type: "image",
+        src: "93.png"
+    },
+
+    {
+        id: "image-098",
+        type: "image",
+        src: "94.png"
+    },
+
+    {
+        id: "image-099",
+        type: "image",
+        src: "95.png"
+    },
+
+    {
+        id: "image-100",
+        type: "image",
+        src: "96.png"
+    },
+
+    {
+        id: "image-101",
+        type: "image",
+        src: "97.png"
+    },
+
+    {
+        id: "image-102",
+        type: "image",
+        src: "98.png"
+    },
+
+    {
+        id: "image-103",
+        type: "image",
+        src: "99.png"
+    },
+
+    {
+        id: "image-104",
+        type: "image",
+        src: "100.png"
+    },
+
+    {
+        id: "image-105",
+        type: "image",
+        src: "101.png"
+    },
+
+    {
+        id: "image-106",
+        type: "image",
+        src: "102.png"
+    },
+
+    {
+        id: "image-107",
+        type: "image",
+        src: "103.png"
+    },
+
+    {
+        id: "image-108",
+        type: "image",
+        src: "104.png"
+    },
+
+    {
+        id: "image-109",
+        type: "image",
+        src: "105.png"
+    },
+
+    {
+        id: "image-110",
+        type: "image",
+        src: "106.png"
+    },
+
+    {
+        id: "image-111",
+        type: "image",
+        src: "107.png"
+    },
+
+    {
+        id: "image-112",
+        type: "image",
+        src: "108.png"
+    },
+
+    {
+        id: "image-113",
+        type: "image",
+        src: "109.png"
+    },
+
+    {
+        id: "image-114",
+        type: "image",
+        src: "110.png"
+    },
+
+    {
+        id: "image-115",
+        type: "image",
+        src: "111.png"
+    },
+
+    {
+        id: "image-116",
+        type: "image",
+        src: "112.png"
+    },
+
+    {
+        id: "image-117",
+        type: "image",
+        src: "113.png"
+    },
+
+    {
+        id: "image-118",
+        type: "image",
+        src: "114.png"
+    },
+    
 
 ];
